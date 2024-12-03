c9973aebbc9a704497472ff3bd53acb93f1a5581
tmorgan1 <tmorgan1@netjets.com>
2024-12-03 12:28:48 -0500

    .

--- day_three.go
+++ day_three.go
@@ -3,9 +3,15 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"regexp"
 	"strconv"
+)
+
+var (
+	mulRegexp = regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+	mulEnabledRegexp = regexp.MustCompile(`mul\((\d+),(\d+)\)|do\(\)|don\'t\(\)`)
 )
 
 func dayThree() {
@@ -14,11 +20,12 @@ func dayThree() {
 	defer file.Close()
 
 	getMulSum(file)
+	file.Seek(0, io.SeekStart)
+	getMulSumEnabled(file)
 }
 
 func getMulSum(file *os.File) {
 	scanner := bufio.NewScanner(file)
-	mulRegexp := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
 	var muls []int
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -31,7 +38,31 @@ func getMulSum(file *os.File) {
 			muls = append(muls, x*y)
 		}
 	}
-	fmt.Printf("Mul Sum: %d\n", sum(muls))
+	fmt.Printf("Mul Sum\t%d\n", sum(muls))
+}
+
+func getMulSumEnabled(file *os.File) {
+	scanner := bufio.NewScanner(file)
+	var muls []int
+	enabled := true
+	for scanner.Scan() {
+		line := scanner.Text()
+		matches := mulEnabledRegexp.FindAllStringSubmatch(line, -1)
+		for _, match := range matches {
+			if match[0] == "don't()" {
+				enabled = false
+			} else if match[0] == "do()" {
+				enabled = true
+			} else if enabled {
+				x, err := strconv.Atoi(match[1])
+				check(err)
+				y, err := strconv.Atoi(match[2])
+				check(err)
+				muls = append(muls, x*y)
+			}
+		}
+	}
+	fmt.Printf("Mul Sum Switch:\t%d\n", sum(muls))
 }
 
 func sum(nums []int) int {
